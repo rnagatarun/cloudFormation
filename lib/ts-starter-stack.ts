1e22af13323770caa28e56786c3512ed6d267181
NagaTarun <rnagatarun@gmail.com>
2025-01-22 17:48:39 +0530

    cloud formation bucket creation

--- lib/ts-starter-stack.ts
+++ lib/ts-starter-stack.ts
@@ -1,16 +1,17 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
 
 export class TsStarterStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'TsStarterQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new Bucket(this, 'TsBucket', {
+      lifecycleRules:[
+        {
+          expiration: cdk.Duration.days(2)
+        }
+      ]
+    })
   }
 }
